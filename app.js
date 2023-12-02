fc2ed07afabb878774f74c1d3a47254cbf010738
Bishar Abdinur Mohamed <134773924+bisharr@users.noreply.github.com>
2023-12-02 07:58:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -13,12 +13,39 @@ let page = 1;
 async function searchImages() {
   inputData = SearchInputEl.value;
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
-  console.log(url);
+
   const response = await fetch(url);
   const data = await response.json();
+  if (page === 1) {
+    searchResultEl.innerHTML = '';
+  }
+  const results = data.results;
+  results.map((result) => {
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('search-result');
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target = '_blank';
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResultEl.appendChild(imageWrapper);
+  });
+  page++;
+  if (page > 1) {
+    showMoreButton.style.display = 'block';
+  }
 }
 
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
+  searchImages();
+});
+
+showMoreButton.addEventListener('click', () => {
   searchImages();
 });
